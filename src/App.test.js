91545cc7934dc28ff9467b2852c093701f02e85f
Jordan <jordanleelong5@gmail.com>
2020-11-24 19:17:35 -0800

    wrote tests for app render, and has state set to null, still need to finish api call test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+// import App from './App';
+import App from './App.js'
+import {shallow} from 'enzyme';
+import fetchMock from 'fetch-mock'
+import moxios from 'moxios'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const nextTick = async () => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 0)
+  })
+}
+
+const dummyUser = {
+  id: 1, 
+  name: 'Jack Franklin', 
+  website: 'https::javascriptplayground.com'
+}
+
+describe('App component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+    moxios.install()
+
+  })
+
+  afterEach(() => {
+    // wrapper.unmount();
+    moxios.uninstall();
+  })
+
+  test('component renders without an error', () => {
+    const component = wrapper.find('.app-container'); 
+    expect(component.exists()).toBe(true);
+  }); 
+
+  test('has state user set to null', () => {
+    const text = wrapper.find('.loading-paragraph');
+    expect(text.text()).toEqual('Loading!') 
+  })
+
+  test('h4 contains users name from fetch', async () => {
+  const usersName = dummyUser.name
+
+  moxios.wait(() => {
+    const request = moxios.requests.mostRecent(); 
+    request.respondWith({
+      status: 200, 
+      response: usersName
+    })
+  })
+  return wrapper.instance()
+  .then(() => {
+    const oldState = wrapper.state()
+    expect(oldState.user).toBe(usersName)
+  })
+   
+  })
+
+})
